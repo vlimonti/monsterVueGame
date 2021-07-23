57468194639c44d5eaa4e41952fa00d259d070f5
Victor Limonti <victor_limonti@hotmail.com>
2021-07-23 18:26:10 -0300

    Attack and Heal

--- app.js
+++ app.js
@@ -12,9 +12,30 @@ new Vue({
     },
     methods: {
         startGame() {
-            this.running = true;
-            this.playerLife = 100;
-            this.monsterLife = 100;
+            this.running = true
+            this.playerLife = 100
+            this.monsterLife = 100
+        },
+        attack(especial) {
+            this.hurt('playerLife', 7, 12, false)
+            this.hurt('monsterLife', 5, 10, especial)
+        },
+        hurt(atributte, min, max, especial) {
+            const plus = especial ? 5 : 0
+            const hurt = this.getRandom(min + plus, max + plus)
+            this[atributte] = Math.max(this[atributte] - hurt, 0)
+        },
+        getRandom(min, max) {
+            const value = Math.random() * (max - min) + min
+            return Math.round(value)
+        },
+        healAndHurt() {
+            this.heal(10, 15)
+            this.hurt('playerLife', 7, 12, false)
+        },
+        heal(min, max) {
+            const health = this.getRandom(7, 12)
+            this.playerLife = Math.min(this.playerLife + health, 100)
         }
     },
     watch: {
