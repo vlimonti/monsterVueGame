79e7623b2d359645f31aa3d4d6d122474cf43a88
Victor Limonti <victor_limonti@hotmail.com>
2021-07-25 15:49:00 -0300

    Log implementation

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ new Vue({
     data: {
         running: false,
         playerLife: 100,
-        monsterLife: 100
+        monsterLife: 100,
+        logs: []
     },
     computed: {
         hasResult() {
@@ -15,30 +16,41 @@ new Vue({
             this.running = true
             this.playerLife = 100
             this.monsterLife = 100
+            this.logs = []
         },
         attack(especial) {
-            this.hurt('playerLife', 7, 12, false)
-            this.hurt('monsterLife', 5, 10, especial)
+            this.hurt('monsterLife', 5, 10, especial, 'Jogador', 'Monstro', 'player')
+            if (this.monsterLife > 0) {
+                this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster')
+            }
         },
-        hurt(atributte, min, max, especial) {
+        hurt(atributte, min, max, especial, source, target, cls) {
             const plus = especial ? 5 : 0
             const hurt = this.getRandom(min + plus, max + plus)
             this[atributte] = Math.max(this[atributte] - hurt, 0)
+            this.registerLog(`${source} atingiu ${target} com ${hurt}.`, cls)
+        },
+        healAndHurt() {
+            this.heal(10, 15)
+            this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster')
+        },
+        heal(min, max) {
+            const health = this.getRandom(min, max)
+            this.playerLife = Math.min(this.playerLife + health, 100)
+            this.registerLog(`Jogador ganhou força de ${health}`, 'player')
         },
         getRandom(min, max) {
             const value = Math.random() * (max - min) + min
             return Math.round(value)
         },
-        healAndHurt() {
-            this.heal(10, 15)
-            this.hurt('playerLife', 7, 12, false)
-        },
-        heal(min, max) {
-            const health = this.getRandom(7, 12)
-            this.playerLife = Math.min(this.playerLife + health, 100)
+        registerLog(text, cls) {
+            this.logs.unshift({ text, cls })
         }
+
     },
     watch: {
-
+        hasResult(value) {
+            if (value) this.running = false
+        }
     }
 })
